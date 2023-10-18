76edf1cd1236a60b1c92988fd72b82b28f6c3ce7
Jamesson Faria <jamessonjr@gmail.com>
2023-10-18 15:33:10 -0300

    Implementacao da busca otimiza usando o algoritmo hill climb

---
+++ algoritmos_de_otimizacao/calendario_voos/hill_climb.py
@@ -0,0 +1,8 @@
+import six
+import sys
+sys.modules['sklearn.externals.six'] = six
+import mlrose
+
+
+def get_solucao(problema):
+    return mlrose.hill_climb(problema, random_state=3)
--- algoritmos_de_otimizacao/calendario_voos/representacao_problema.py
+++ algoritmos_de_otimizacao/calendario_voos/representacao_problema.py
@@ -1,3 +1,4 @@
+from algoritmos_de_otimizacao.calendario_voos import hill_climb, fitness_function
 
 pessoas = [('Lisboa', 'LIS'),
            ('Madrid', 'MAD'),
@@ -7,29 +8,17 @@ pessoas = [('Lisboa', 'LIS'),
            ('Londres', 'LHR')]
 
 destino = 'FCO'
-
 voos = {}
+
 for linha in open('flights.txt'):
     origem, destino, saida, chegada, preco = linha.split(',')
     voos.setdefault((origem, destino), [])
     voos[(origem, destino)].append((saida, chegada, int(preco)))
 
+agenda = [1, 7, 3, 1, 1, 1, 6, 3, 2, 4, 5, 3]
+valor = fitness_function.fitness_function(agenda)
 
-def imprimir_voos(agenda):
-    id_voo = -1
-    total_preco = 0
-    for i in range(len(agenda) // 2):
-        nome = pessoas[i][0]
-        origem = pessoas[i][1]
-        id_voo += 1
-        ida = voos[(origem, destino)][agenda[id_voo]]
-        total_preco += ida[2]
-        id_voo += 1
-        volta = voos[(destino, origem)][agenda[id_voo]]
-        total_preco += volta[2]
-        print('%10s%10s %5s-%5s %3s %5s-%5s %3s' % (nome, origem, ida[0], ida[1], ida[2],
-                                                    volta[0], volta[1], volta[2]))
-    print('Preço total: ', total_preco)
+problema = fitness_function.get_problema()
 
-agenda = [1,1, 3,2, 7,1, 6,3, 2,4, 5,3]
-imprimir_voos(agenda)
+melhor_solucao, melhor_custo = hill_climb.get_solucao(problema)
+fitness_function.imprimir_voos(melhor_solucao)
